a28578dfc6411a224f52d91bf59f7ca398a570b4
“ajid” <muhajidachmad@gmail.com>
2024-08-13 20:37:35 +0700

    Initial project

---
+++ src/main/java/com/bvk/test/backend_organization/BackendOrganizationApplication.java
@@ -0,0 +1,13 @@
+package com.bvk.test.backend_organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOrganizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOrganizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bvk/test/backend_organization/BackendOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvk.test.backend_organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOrganizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
