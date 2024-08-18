30115c007e20f40c05a61cde417e8778d2ee755c
“ajid” <muhajidachmad@gmail.com>
2024-08-18 23:14:58 +0700

    Add api json endpoint

--- src/main/java/com/bvk/test/backend_organization/BackendOrganizationApplication.java
+++ src/main/java/com/bvk/test/backend_organization/BackendOrganizationApplication.java
@@ -8,12 +8,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendOrganizationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendOrganizationApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 //	@Bean
--- src/main/java/com/bvk/test/backend_organization/controllers/UserController.java
+++ src/main/java/com/bvk/test/backend_organization/controllers/UserController.java
@@ -8,10 +8,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,4 +24,12 @@ public class UserController {
 
         return ResponseEntity.ok(authenticationService.registration(request));
     }
+
+//    @GetMapping
+//    public String profile(Principal principal) {
+//
+//        log.info("principadate -> {}", principal.toString());
+//
+//        return principal.getName();
+//    }
 }
